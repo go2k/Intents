8d72ca9036f730e36ae931b3cd42e1f144bd2cb0
Michael König <m.koenig@go2k.de>
2018-01-11 10:43:32 +0100

    initialer commit

--- app/src/main/java/sabel/com/intents/ViewActivity.java
+++ app/src/main/java/sabel/com/intents/ViewActivity.java
@@ -2,12 +2,30 @@ package sabel.com.intents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ViewActivity extends AppCompatActivity {
+
+    private TextView tvName;
+    private Button btnZurueck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        CreateObjects();
+        CreateListener();
+
+    }
+
+    private void CreateListener() {
+
+    }
+
+    private void CreateObjects() {
+        tvName = findViewById(R.id.etName);
+        btnZurueck = findViewById(R.id.btnZurueck);
     }
 }
